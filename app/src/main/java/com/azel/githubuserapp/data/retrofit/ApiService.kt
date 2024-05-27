55d4d37494eb399f8758e195142c4a7e5e51149b
Syahriza <78152595+Rizautomo@users.noreply.github.com>
2024-05-27 17:15:01 +0700

    Update ApiService.kt

--- app/src/main/java/com/azel/githubuserapp/data/retrofit/ApiService.kt
+++ app/src/main/java/com/azel/githubuserapp/data/retrofit/ApiService.kt
@@ -11,20 +11,20 @@ import retrofit2.http.*
 interface ApiService {
     //search User: https://api.github.com/search/users?q=sidiqpermana
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token")
     fun getUsers (
         @Query("q") Query: String
     ): Call<GithubResponse>
 
     @GET("users/{username}")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ")
     fun getDetailUser(@Path("username") username: String): Call<DetailUserResponse>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token")
     fun getListFollowers(@Path("username") username: String): Call<List<ItemsItem>>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token")
     fun getListFollowing(@Path("username") username: String): Call<List<ItemsItem>>
 }
